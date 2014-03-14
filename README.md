46126c09f936788b2e03045dbf27cfb667695b3e
bouktin <thomasfp.blanchard@gmail.com>
2014-03-14 18:01:17 -0400

    removal of obsolete scripts

--- README.md
+++ README.md
@@ -2,8 +2,4 @@ Scripts
 =======
 
 Some useful and less useful scripts in Perl, Bash, Python or other scripted language
-
-List
-=======
-
-dell-warranty-api.pl  fetches the DELL warranty information for the service tag given as parameter
+See Scripts/README.md
---
+++ scripts/perl-simple-check-modules.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use local::lib;
+#use lib '/home/nagios/perl5/lib/perl5';
+#print "\$0=@ARGV[0]\t\$1=@ARGV[1]\t\$2=$ARGV[2]\n";
+#if ( @ARGV[0] =~ /local/ ) { use local::lib; shift @ARGV; }
+#else {print "don't use local::lib\n";}
+
+foreach my $module ( @ARGV ) {
+  eval "require $module";
+  if ( !$@ ) { printf( "%-20s: %s\n", $module, $module->VERSION ) ; }
+  else { printf("%-20s: NOT_FOUND\n", $module); }
+}
+
+exit 0;
+
+__END__
+
+force a different library:
+
+perl -e 'use strict; use local::lib; use lib "/home/nagios/lib/perl5"; my $module="Tree::Simple::VisitorFactory"; eval "require $module"; if ( !$@ ) { printf( "%-20s: %s\n", $module, $module->VERSION ) ; } else { printf("%-20s: NOT_FOUND\n", $module); }'
+
+perl -e 'use strict; use local::lib; use lib "/home/rt/lib/"; use lib "/home/rt/lib/perl5/";
+my @lst = qw/ Net::SSLeay Cyrpt::SSLeay /;
+foreach my $module ( @lst ) {
+eval "require $module";
+if ( !$@ ) { printf( "%-20s: %s\n", $module, $module->VERSION ) ; }
+else { printf("%-20s: NOT_FOUND\n", $module); }
+}'
